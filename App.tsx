cf413b1116afdc0a18f69811b60be3dab4be99d4
Your Name <pablobeernardo@gmail.com>
2023-06-13 20:58:23 -0300

    sdfh

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import TeamEntity from './src/entities/team_entity';
@@ -6,13 +6,12 @@ import { Image } from 'expo-image';
 
 export default function App() {
 
-  //Linha do state
   const [teams, setTeam] = useState<TeamEntity[]>([]);
 
 
   useEffect(() => {
     var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer ");
+    myHeaders.append("Authorization", "Bearer test_b7555f93259e437fd7031592dce53a");
 
     var requestOptions = {
       method: 'GET',
@@ -21,7 +20,7 @@ export default function App() {
 
     let teamsPosition: TeamEntity[] = [];
 
-    fetch("https://api.api-futebol.com.br/v1/campeonatos/10/tabela", requestOptions)
+    fetch( "https://api.api-futebol.com.br/v1/campeonatos/10/tabela" ,requestOptions)
       .then(response => response.text())
       .then(result => JSON.parse(result))
       .then(dataJson => {
@@ -32,7 +31,9 @@ export default function App() {
             position: team['posicao'], 
             team_shield_url: team['time']['escudo'], 
             team_name: team['time']['nome_popular'],
-            team_points: team['pontos']
+            team_points: team['pontos'],
+            team_gols_pro: team['gols_pro'],
+            team_saldo_gols: team['saldo_gols']
           };
 
           teamsPosition.push(dataTeam);
@@ -45,18 +46,37 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tabela Brasileirão série A</Text>
+      <Text style={styles.title}>Tabela Brasileirão série B</Text>
       <View style={styles.table}>
+      <View style={styles.itens}>
+             <Text style={styles.pts}>PTS</Text>
+             <Text style={styles.gl}>GLS</Text>  
+             <Text style={styles.SG}>SG</Text>
+     </View>
         <FlatList
           data={teams}
           keyExtractor={(item) => item.id.toString()}
           renderItem={(team) =>
             <View style={styles.item}>
+
+             
+
+           
+
+
+
+
+
               <Image style={styles.team_shield} source={team.item.team_shield_url} />
-              <Text style={styles.team_position}>{team.item.position}</Text>
+              <Text style={styles.position}>{team.item.position}</Text>
               <Text style={styles.team_name}>{team.item.team_name}</Text>
-              <Text style={styles.team_position}>{team.item.team_points}</Text>
+              <Text style={styles.position}>{team.item.team_points}</Text>
+              <Text style={styles.position}>{team.item.team_gols_pro}</Text>
+              <Text style={styles.position}>{team.item. team_saldo_gols}</Text>
             </View>
+
+            
+            
           }
 
         />
@@ -68,15 +88,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'flex-start',
     marginTop: 30,
     marginBottom: 16,
     marginHorizontal: 16,
+    borderRadius:10
   },
   title: {
-    fontSize: 30,
+    fontSize: 35,
     fontWeight: '700',
     marginBottom: 16,
     marginTop: 30
@@ -94,17 +115,44 @@ const styles = StyleSheet.create({
     height: 50
   },
   team_shield: {
-    width: 30,
+    width: 35,
     height: 30
   },
   team_name:{
     fontSize: 20,
     width: 150,
     textAlign: 'center',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    borderRadius:20
   },
-  team_position:{
+  position:{
     width: 30,
     fontSize: 20,
+  },
+
+  itens:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    paddingHorizontal: 16,
+    
+    height: 20,
+    marginHorizontal:35
+  },
+  pts:{
+    width: 30,
+    fontSize: 5,
+  },
+  gl:{
+    width: 30,
+    fontSize: 5, 
+  },
+  SG:{
+    width: 30,
+    fontSize: 5, 
   }
+
+
+ 
+ 
 });
--- src/entities/team_entity.tsx
+++ src/entities/team_entity.tsx
@@ -4,4 +4,7 @@ export default class TeamEntity{
     team_shield_url:string;
     team_name:string;
     team_points:number;
+    team_gols_pro:number;
+    team_saldo_gols:number;
+ 
 }
